f1460970cadad9b4fda4f5f91517b34fbde9617a
salinda-yasas-99 <salindasym@gmail.com>
2024-01-03 19:15:23 +0530

    adding changes in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,10 @@ import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import Image from "next/image";
 import logoImg from "../../assets/logo.png";
+import { FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -30,7 +34,18 @@ const Footer = () => {
       {/* row2 */}
       <div className="text-[#fff] mx-20">Custom Software Development</div>
       <div className="text-[#fff] mx-20">Home</div>
-      <div className="text-[#fff] mx-20">1</div>
+      <div className="text-[#fff] mx-20">
+        <div className="flex py-1 bg-[#fff] rounded-full items-center justify-center">
+          <input
+            type="text"
+            placeholder="Enter your email address"
+            className="h-4"
+          />
+          <button className="px-6 py-3 bg-gradient-to-r from-[#3c83c6] to-[#92cbf5] rounded-full">
+            Subscribe
+          </button>
+        </div>
+      </div>
       {/* row3 */}
       <div className="text-[#fff] mx-20">Mobile Application Development</div>
       <div className="text-[#fff] mx-20">About</div>
@@ -38,10 +53,31 @@ const Footer = () => {
       {/* row4 */}
       <div className="text-[#fff] mx-20">Web Application Development</div>
       <div className="text-[#fff] mx-20">Services</div>
-      <div className="flex row-span-2"></div>
+      <div className="flex row-span-2">
+        <div className="flex space-x-4 justify-center items-center mx-20">
+          <div className="bg-[#4a91ce] p-2 rounded-md">
+            <FaFacebookF color="white" />
+          </div>
+          <div className="bg-[#4a91ce] p-2 rounded-md">
+            <FaXTwitter color="white" />
+          </div>
+          <div className="bg-[#4a91ce] p-2 rounded-md">
+            <IoLogoInstagram color="white" />
+          </div>
+          <div className="bg-[#4a91ce] p-2 rounded-md">
+            <FaLinkedinIn color="white" />
+          </div>
+        </div>
+      </div>
       {/* row5 */}
-      <div className="text-[#fff] mx-20">UI/UX Design & Development</div>
+      <div className="text-[#fff] mx-20 ">UI/UX Design & Development</div>
       <div className="text-[#fff] mx-20">Contacts</div>
+      <div className="col-span-3 border-b-[#fff] border-b-2 my-4 mx-20 h-10" />
+      <div className="flex justify-center items-center col-span-3 text-[#9ee3fc] mx-20 my-2">
+        Copyright © 2023 Innov8Fusion All Rights Reserved
+        <span className="text-[#fff] px-2">|</span> Terms and Conditions
+        <span className="text-[#fff] px-2">|</span> Privacy Policy
+      </div>
     </div>
   );
 };
